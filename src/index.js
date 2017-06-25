0b8480e3b011c1ceb0c710f2536a3068d67f549a
aurbanski <aurbanski27@gmail.com>
2017-06-25 11:35:57 -0600

    Adding the whole app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {Router} from './components/router'
+import './index.css';
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+registerServiceWorker();
